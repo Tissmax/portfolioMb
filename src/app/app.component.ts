ce11fb5df66ab525bba92f5ac055f874beec46de
Mathis <mathis.basier@gmail.com>
2024-03-14 17:36:01 +0100

    toogleProjet + Parcours

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { ProjetCardsComponent } from './projet-cards/projet-cards.component'
 import { ParcoursCardsComponent } from './parcours-cards/parcours-cards.component';
 import { HttpClientModule } from '@angular/common/http';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -28,38 +29,25 @@ export class AppComponent {
   projetsIsShown: boolean=false;
   
   //on regarde sur quelle bouton l'utilisateur clic
-  projetsIsClicked: number = 0;
-  parcoursIsClicked: number = 0;
+  toogleParcours: boolean = false;
+  toogleProjet: boolean = false;
 
-  //on associe le bouton à la div à afficher 
-  //et on empêche l'utilisateur d'afficher l'autre div
-  inc(i: number) {
-    i++;
-    console.log(i);
-    
-  }
-  
-  showingCard(activeCard: number, otherCard:number, showingCard: boolean) {
-    if (otherCard == 0 ? true : false && activeCard == 1) {
-      showingCard = true;
-
-    }
-    if (activeCard == 2) {
-      showingCard = false;
-      activeCard = 0
-
-    }
-
-  }
-  
-  showingProjets() {
-    if (this.parcoursIsClicked == 1 ? false : true && this.projetsIsClicked == 1) {
-      this.projetsIsShown = true;
-      this.parcoursIsShown = false;
-
+  showProjet() {
+    if (this.toogleProjet == false && this.toogleParcours == false) {
+      this.toogleProjet = true;
+    } else if (this.toogleProjet == true) {
+      this.toogleProjet = false;
     }
   }
-
+ 
+  showParcours() {
+    if (this.toogleParcours == false  && this.toogleProjet == false) {
+      this.toogleParcours = true;
+    } else if (this.toogleParcours == true) {
+      this.toogleParcours = false;
+    }
+  }
+ 
   onHover() {
     this.bonjoStyle="color: #0C1325;";
     this.cercleGrand="cercleGrand hoverCercle";
