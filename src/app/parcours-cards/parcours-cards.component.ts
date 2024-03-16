1984929eff1234b5fe74fcbdde111619aee97975
Mathis <mathis.basier@gmail.com>
2024-03-16 10:16:06 +0100

    effet click card

--- src/app/parcours-cards/parcours-cards.component.ts
+++ src/app/parcours-cards/parcours-cards.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { Card } from '../card';
 import { CardDataService } from '../card-data.service';
 import { CommonModule } from '@angular/common';
+import { Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-parcours-cards',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,          
+  ],
   providers: [CardDataService],
   templateUrl: './parcours-cards.component.html',
   styleUrl: './parcours-cards.component.scss'
@@ -16,10 +18,40 @@ export class ParcoursCardsComponent {
 
   cards!: Card[];
   
-  constructor(private cardService: CardDataService) { }
+  constructor(private cardService: CardDataService, private renderer:Renderer2) { }
 
   ngOnInit(): void {
     this.cards = this.cardService.getCardParcours();
   }
 
+  
+  i:number=0;
+  
+  redistributionCard(cardId: number) {
+
+//n est l'indice de la carte
+//on incrémente pour savoir combien de fois l'utilisateur click
+    this.i++;
+    let a = cardId+1;
+    let b = cardId+2;
+    let c = cardId+3;
+
+
+    if (this.i + cardId == 5) {
+      this.renderer.addClass(document.getElementById("card"+cardId), 'left1');
+    }
+    if (this.i+cardId+1 == 6) {
+      this.renderer.addClass(document.getElementById("card"+a), 'left2');
+      this.renderer.removeClass(document.getElementById("card"+a), 'left1');
+    }
+    if (this.i+cardId+2 == 7) {
+      this.renderer.addClass(document.getElementById("card"+b), 'left3');
+      this.renderer.removeClass(document.getElementById("card"+b), 'left2');
+    }
+    if (this.i+cardId+3 == 8) {
+      this.renderer.addClass(document.getElementById("card"+c), 'left4');
+      this.renderer.removeClass(document.getElementById("card"+c), 'left3');
+    }
+    console.log(this.i)
+  }
 }
